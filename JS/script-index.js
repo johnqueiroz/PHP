c5cf881b677b5d53b92f73b8b75b6014bb89516b
johnqueiroz <johnemerson67@gmail.com>
2023-04-27 16:45:21 -0300

    Falta organizar o modal dos lembretes.

--- JS/script-index.js
+++ JS/script-index.js
@@ -4,3 +4,18 @@ document.querySelector('#menu-btn ').addEventListener('click', function(){
 
     document.querySelector('#menu-icon').classList.toggle("active")
 })
+
+const modal = document.querySelector("dialog")
+
+function abrirModal(){
+    modal.showModal()
+}
+
+function adicionar(){
+    let text = document.getElementById("lembrete").value
+    let lista = document.getElementById("lista-lembrete").innerHTML
+
+    lista += "<li>" +text+ "</li>"
+
+    document.getElementById("lista-lembrete").innerHTML = lista
+}
